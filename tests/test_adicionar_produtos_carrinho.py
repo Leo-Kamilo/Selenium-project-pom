4ffad7e3eee23b0096d6bd653c11e9a3ba50805f
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-10-16 22:07:04 -0300

    Aula 27 v1

---
+++ tests/test_adicionar_produtos_carrinho.py
@@ -0,0 +1,24 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.implicitly_wait(5) 
+driver.maximize_window()
+driver.get('https://www.saucedemo.com/')
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+time.sleep(2)
+driver.find_element(By.ID, "login-button").click()
+time.sleep(2)
+
+driver.find_element(By.XPATH, '//*[@class="inventory_item_name " and text()="Sauce Labs Backpack"]').click
+time.sleep(2)
+driver.find_element(By.XPATH, "//*[text()='Add to cart']").click()
+time.sleep(2)
+
+driver.find_element(By.XPATH, "//*[@class='shopping_cart_link']").click()
+time.sleep(2)
+
+assert driver.find_element(By.XPATH, '//*[@class="inventory_item_name " and text()="Sauce Labs Backpack"]').is_displayed()
