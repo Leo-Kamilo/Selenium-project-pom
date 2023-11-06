77d6dc4c1c2eb87e61de5bcbf6414621b8cc0a04
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-11-06 20:08:57 -0300

    Aula 36 finalizada

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,8 @@
 import conftest
 from selenium.webdriver.common.by import By
+from pages.base_page import BasePage
 
-class LoginPage:
+class LoginPage(BasePage):
 
     def __init__(self):
        self.driver = conftest.driver
@@ -11,6 +12,6 @@ class LoginPage:
 
 
     def fazer_login(self, usuario, senha):
-        self.driver.find_element(*self.username_field).send_keys(usuario)
-        self.driver.find_element(*self.password_field).send_keys(senha)
-        self.driver.find_element(*self.login_button).click()
+        self.escrever(self.username_field, usuario)
+        self.escrever(self.password_field, senha)
+        self.clicar(self.login_button)
