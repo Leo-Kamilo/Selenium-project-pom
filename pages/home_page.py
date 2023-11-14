ba456ea15657168cc1d46ad6c04fc73a791ceff4
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-11-14 12:47:24 -0300

    Aula 40 finalizada

--- pages/home_page.py
+++ pages/home_page.py
@@ -8,6 +8,16 @@ class HomePage(BasePage):
     def __init__(self):
        self.driver = conftest.driver
        self.titulo_pagina = (By.XPATH, "//span[@class='title']")
+       self.item_iventario = (By.XPATH,'//*[@class="inventory_item_name " and text()="{}"]')
+       self.adicionar_carrinho = (By.XPATH, "//*[text()='Add to cart']")   
+
+       
+        
 
     def verificar_login_com_sucesso(self):
         self.verificar_se_elemento_existe(self.titulo_pagina)
+
+    def adicionar_ao_carrinho(self, nome_item):
+        item = (self.item_iventario[0], self.item_iventario[1].format(nome_item))  
+        self.clicar(item)   
+        self.clicar(self.adicionar_carrinho)
--- tests/test_ct01_adicionar_produtos_carrinho.py
+++ tests/test_ct01_adicionar_produtos_carrinho.py
@@ -2,6 +2,8 @@ import time
 import pytest
 from selenium.webdriver.common.by import By
 import conftest
+from pages.home_page import HomePage
+from pages.login_page import LoginPage
 
 
 @pytest.mark.usefixtures('setup_teardown')
@@ -9,18 +11,15 @@ import conftest
 class TestCT01:
     def test_ct01_adicionar_produtos_carrinho(self):
         driver = conftest.driver
-        #Faz Login
-        driver.find_element(By.ID, "user-name").send_keys("standard_user")
-        driver.find_element(By.ID, "password").send_keys("secret_sauce")
-        time.sleep(2)
-        driver.find_element(By.ID, "login-button").click()
-        time.sleep(2)
+        login_page = LoginPage()
+        home_page = HomePage()
+        
+        #Faz login
+        login_page.fazer_login("standard_user", "secret_sauce")
 
         #Adiciona a mochila no carrinho
-        driver.find_element(By.XPATH, '//*[@class="inventory_item_name " and text()="Sauce Labs Backpack"]').click()
-        time.sleep(2)
-        driver.find_element(By.XPATH, "//*[text()='Add to cart']").click()
-        time.sleep(2)
+        home_page.adicionar_ao_carrinho("Sauce Labs Backpack")
+       
 
         #Verifica se a mochila foi adicionada
         driver.find_element(By.XPATH, "//*[@class='shopping_cart_link']").click()
