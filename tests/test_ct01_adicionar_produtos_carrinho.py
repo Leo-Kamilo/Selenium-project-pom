66dbc892aad86b65195561d2820293bd2574b44d
Leonardo Camilo <leonardo100camilo@gmail.com>
2023-11-27 12:59:54 -0300

    Aula 42 final

--- tests/test_ct01_adicionar_produtos_carrinho.py
+++ tests/test_ct01_adicionar_produtos_carrinho.py
@@ -1,7 +1,4 @@
-import time
 import pytest
-from selenium.webdriver.common.by import By
-import conftest
 from pages.carrinho_page import CarrinhoPage
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
@@ -11,30 +8,32 @@ from pages.login_page import LoginPage
 @pytest.mark.carrinho
 class TestCT01:
     def test_ct01_adicionar_produtos_carrinho(self):
-        driver = conftest.driver
         login_page = LoginPage()
         home_page = HomePage()
         carrinho_page = CarrinhoPage()
+
+        produto_1 = "Sauce Labs Backpack"
+        produto_2 = "Sauce Labs Bolt T-Shirt"
         
         #Faz login
         login_page.fazer_login("standard_user", "secret_sauce")
 
         #Adiciona a mochila no carrinho
-        home_page.adicionar_ao_carrinho("Sauce Labs Backpack")
+        home_page.adicionar_ao_carrinho(produto_1)
        
 
         #Verifica se a mochila foi adicionada
         home_page.acessar_carrinho()
-        carrinho_page.verificar_produto_carrinho_existe("Sauce Labs Backpack")
+        carrinho_page.verificar_produto_carrinho_existe(produto_1)
 
 
          #Volta pra tela de produtos
         carrinho_page.clicar_continuar_comprando()
 
          #Adiciona a mais um item ao carrinho
-        home_page.adicionar_ao_carrinho("Sauce Labs Bolt T-Shirt")
+        home_page.adicionar_ao_carrinho(produto_2)
 
          #Verifica se os 2 itens estão no carrinho
         home_page.acessar_carrinho()
-        carrinho_page.verificar_produto_carrinho_existe("Sauce Labs Backpack")
-        carrinho_page.verificar_produto_carrinho_existe("Sauce Labs Bolt T-Shirt")
+        carrinho_page.verificar_produto_carrinho_existe(produto_1)
+        carrinho_page.verificar_produto_carrinho_existe(produto_2)
